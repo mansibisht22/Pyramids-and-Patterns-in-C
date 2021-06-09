63badc0ea2a0506f1d9056838246e5c8f58ac9aa
Mansi Bisht <84663893+mansibisht22@users.noreply.github.com>
2021-06-09 08:01:06 +0530

    Add files via upload

---
+++ ptrn2.c
@@ -0,0 +1,25 @@
+/* half pyramid of nos
+1
+12
+123
+1234
+12345
+*/
+
+#include <stdio.h>
+int main()
+{
+    int i, j, n;
+    printf("enter no. of rows to obtain pattern: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; ++i)
+    {
+        for (j = 1; j <= i; ++j)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
