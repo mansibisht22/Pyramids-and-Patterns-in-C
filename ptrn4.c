ddd31b9443e406de8b8ebe93d7ed0b9045717cf0
Mansi Bisht <84663893+mansibisht22@users.noreply.github.com>
2021-06-10 08:05:33 +0530

    Add files via upload

---
+++ ptrn4.c
@@ -0,0 +1,26 @@
+/* Hald pyramid of nos
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+*/
+
+#include<stdio.h>
+int main()
+{
+    int n, i, j;
+    printf("enter no. of rows: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
