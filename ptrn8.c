c9d619fec311cbaed5c01db7a2fb89e1d76e7e9a
Mansi Bisht <84663893+mansibisht22@users.noreply.github.com>
2021-06-11 14:25:30 +0530

    Add files via upload

---
+++ ptrn8.c
@@ -0,0 +1,19 @@
+/* full pyramid
+
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+
+*/
+
+#include<stdio.h>
+int main() {
+  int row, i, space, x;
+
+  printf("Enter no. of rows: ");
+  scanf("%d", &row);
+
+  
+}
